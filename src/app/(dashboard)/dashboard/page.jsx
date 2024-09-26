12e2438c54ea82b4a2ed1ab94c2b44b061f1a099
FameILY <bhambidsatyam@gmail.com>
2024-09-26 21:52:15 +0530

    ntc 3

--- src/app/(dashboard)/dashboard/page.jsx
+++ src/app/(dashboard)/dashboard/page.jsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 export default function Dashboard() {
   const {data: session} = useSession()
   const router = useRouter();
-
+//ntc
 
   const redirectYoutube = async () => {
     try {
