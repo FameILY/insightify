4d56bbdff210f369b66046ce139a83ba0bcefaed
FameILY <bhambidsatyam@gmail.com>
2024-10-31 01:16:35 +0530

    redirect uri updated

--- src/app/api/connect/instagram/route.js
+++ src/app/api/connect/instagram/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const params = new URLSearchParams({
     client_id: process.env.FACEBOOK_APP_ID,
-    redirect_uri: process.env.FACEBOOK_APP_REDIRECT_URI,
+    redirect_uri: process.env.NEXTAUTH_URL +""+ process.env.FACEBOOK_APP_REDIRECT_URI,
     config_id: process.env.FACEBOOK_APP_CONFIG_ID,
     scope:
       "instagram_basic,pages_show_list,business_management,read_insights,instagram_manage_insights,pages_read_engagement",
